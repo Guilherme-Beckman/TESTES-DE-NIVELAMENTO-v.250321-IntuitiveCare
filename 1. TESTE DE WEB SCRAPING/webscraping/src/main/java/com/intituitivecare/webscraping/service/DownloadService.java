a2857d063e27251712a385b50d85b397433b2ee1
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-28 16:01:05 -0400

    feat: adiciona método para download de arquivos a partir de links PDF na classe DownloadService

--- 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/DownloadService.java
+++ 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/DownloadService.java
@@ -1,5 +1,8 @@
 package com.intituitivecare.webscraping.service;
 
+import java.io.File;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service 
@@ -7,6 +10,10 @@ public class DownloadService {
 	private String downloadDir;
 	public DownloadService(String downloadDir) {
 		this.downloadDir = downloadDir;
+	}
+	public List<File> downloadFiles(List<String> pdfLinks) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
