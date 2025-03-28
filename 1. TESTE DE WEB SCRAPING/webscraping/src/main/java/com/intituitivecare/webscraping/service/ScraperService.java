7696f7f6556e138e51c055e24379f617faf34c03
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-28 13:57:46 -0400

    feat: implementa método para buscar links de PDF na página

--- 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/ScraperService.java
+++ 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/ScraperService.java
@@ -1,21 +1,40 @@
 package com.intituitivecare.webscraping.service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
 import com.intituitivecare.webscraping.exceptions.scraping.ErrorWhileFetchingPageContentException;
 
-
 @Service
 public class ScraperService {
 	private String URL = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos";
-	public String fetchPageContent() {
+
+	private Document connectToUrl() throws IOException {
+		return Jsoup.connect(URL).get();
+	}
+
+	public List<String> fetchPdfLinks() {
 		try {
-			Document doc = Jsoup.connect(URL).get();
-			return doc.html();
+			List<String> findedLinks = new ArrayList<>();
+			Document doc = connectToUrl();
+			Elements downloadLinks = doc.select("li a");
+
+			for (Element link : downloadLinks) {
+				String href = link.attr("href");
+				String linkText = link.text();
+				if (linkText.contains("Anexo") && href.endsWith(".pdf")) {
+					findedLinks.add(href);
+				}
+			}
+			return findedLinks;
+
 		} catch (IOException e) {
 			throw new ErrorWhileFetchingPageContentException();
 		}
