b8715f8273417636dca680bf77f15d2f2f77c8cb
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-28 13:57:40 -0400

    feat: atualiza teste do ScraperService para verificar links de PDF

--- 1. TESTE DE WEB SCRAPING/webscraping/src/test/java/com/intituitivecare/webscraping/service/ScraperServiceTest.java
+++ 1. TESTE DE WEB SCRAPING/webscraping/src/test/java/com/intituitivecare/webscraping/service/ScraperServiceTest.java
@@ -1,20 +1,25 @@
 package com.intituitivecare.webscraping.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 public class ScraperServiceTest {
 	@Test
-	public void shouldFetchPageContent() {
-		//Instanciar um novo ScraperService
-		ScraperService scraperService = new  ScraperService();
-		
-		//Act: chamar o metodo que pega o conteudo da pagina
-		String content = scraperService.fetchPageContent();
-		
-		//Não pode ser nulo
-		assertNotNull(content, "O conteúdo da página não pode ser null");
-		
+	public void shouldFetchPdfLinks() {
+		// Instanciar um novo ScraperService
+		ScraperService scraperService = new ScraperService();
+
+		// Act: chamar o metodo que pega o conteudo da pagina
+		List<String> pdfLinks = scraperService.fetchPdfLinks();
+
+		// Não pode ser nulo
+		assertNotNull(pdfLinks, "Os links não podem ser null");
+		// A lista deve conter dois elementos
+		assertEquals(2, pdfLinks.size(), "A lista de links deve conter exatamente dois elementos");
+
 	}
 }
