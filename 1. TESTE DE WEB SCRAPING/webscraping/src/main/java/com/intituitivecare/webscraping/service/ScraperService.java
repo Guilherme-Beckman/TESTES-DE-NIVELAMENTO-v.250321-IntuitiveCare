9efa082240553dad43f216a4d0c604310b4414bf
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-28 13:20:52 -0400

    feat: adiciona implementação de metodo para fazer fetch da pagina

--- 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/ScraperService.java
+++ 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/ScraperService.java
@@ -1,13 +1,23 @@
 package com.intituitivecare.webscraping.service;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class ScraperService {
-
+	private String URL = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos";
 	public String fetchPageContent() {
-		
-		return "Não está retornando null";
+		try {
+			Document doc = Jsoup.connect(URL).get();
+			return doc.html();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
