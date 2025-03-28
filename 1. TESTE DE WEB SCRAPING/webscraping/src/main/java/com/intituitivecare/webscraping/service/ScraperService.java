3ac49c48c9f4aaa5a0fd69d1acd5fcbf0a8abc21
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-28 12:08:23 -0400

    feat: implementa fetchPageContent para dar green

--- 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/ScraperService.java
+++ 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/ScraperService.java
@@ -1,5 +1,13 @@
 package com.intituitivecare.webscraping.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ScraperService {
+
+	public String fetchPageContent() {
+		
+		return "Não está retornando null";
+	}
 
 }
