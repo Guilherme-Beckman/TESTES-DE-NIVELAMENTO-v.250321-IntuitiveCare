ad487e9e40cbd4c0a0c5b827fe445feb4c8e2425
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-28 16:53:41 -0400

    feat: implementa método para download de arquivos PDF na classe DownloadService

--- 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/DownloadService.java
+++ 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/DownloadService.java
@@ -1,6 +1,12 @@
 package com.intituitivecare.webscraping.service;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -11,9 +17,40 @@ public class DownloadService {
 	public DownloadService(String downloadDir) {
 		this.downloadDir = downloadDir;
 	}
+	public DownloadService() {
+		this("downloads");
+	}
 	public List<File> downloadFiles(List<String> pdfLinks) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			List<File> downloadedFiles = new ArrayList<>();
+			for (String pdfLink : pdfLinks) {
+				Path downloadPath = Path.of(downloadDir);
+				if(!Files.exists(downloadPath)) {
+					Files.createDirectories(downloadPath);
+				}
+				
+				String fileName = pdfLink.substring(pdfLink.lastIndexOf("/")+1);
+				
+				File outputFile = new File(downloadDir, fileName);
+				
+				try (BufferedInputStream in = new BufferedInputStream(new URL(pdfLink).openStream())){
+					FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+					byte dataBuffer[] = new byte[1024];
+					int bytesRead;
+					
+					while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+						fileOutputStream.write(dataBuffer, 0, bytesRead);
+					}
+					fileOutputStream.close();
+				} 
+				downloadedFiles.add(outputFile);
+			}
+			
+			return downloadedFiles;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
