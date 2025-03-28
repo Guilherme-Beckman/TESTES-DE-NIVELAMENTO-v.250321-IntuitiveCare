01d40f0f3642443d5b9e1681769919c27540fd96
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-28 15:58:19 -0400

    test/feat: adiciona um metodo para limpar o diretorio depois de cada teste

--- 1. TESTE DE WEB SCRAPING/webscraping/src/test/java/com/intituitivecare/webscraping/service/DownloadServiceTest.java
+++ 1. TESTE DE WEB SCRAPING/webscraping/src/test/java/com/intituitivecare/webscraping/service/DownloadServiceTest.java
@@ -1,5 +1,27 @@
 package com.intituitivecare.webscraping.service;
 
-public class DownloadServiceTest {
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.AfterEach;
+public class DownloadServiceTest {
+	
+	private static final String DOWNLOAD_DIR = "download_test";
+	
+	//Primeiro vou precisar de um metodo para limpar tudo quando terminar
+	@AfterEach
+	public void cleanUp() throws Exception{
+		Path downloadPath = Path.of(DOWNLOAD_DIR);
+		
+		if(Files.exists(downloadPath)) {
+			Files.walk(downloadPath)
+			.map(Path::toFile)
+			.forEach(File::delete);
+			Files.deleteIfExists(downloadPath);
+		}
+	}
+
+	
 }
