86bbe4afa0aae344a6a9bf47330721c7b8cc499f
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-28 14:12:51 -0400

    refactor: remove comentários desnecessários

--- 1. TESTE DE WEB SCRAPING/webscraping/src/test/java/com/intituitivecare/webscraping/service/ScraperServiceTest.java
+++ 1. TESTE DE WEB SCRAPING/webscraping/src/test/java/com/intituitivecare/webscraping/service/ScraperServiceTest.java
@@ -10,15 +10,11 @@ import org.junit.jupiter.api.Test;
 public class ScraperServiceTest {
 	@Test
 	public void shouldFetchPdfLinks() {
-		// Instanciar um novo ScraperService
 		ScraperService scraperService = new ScraperService();
-
-		// Act: chamar o metodo que pega o conteudo da pagina
+		
 		List<String> pdfLinks = scraperService.fetchPdfLinks();
-
-		// Não pode ser nulo
+		
 		assertNotNull(pdfLinks, "Os links não podem ser null");
-		// A lista deve conter dois elementos
 		assertEquals(2, pdfLinks.size(), "A lista de links deve conter exatamente dois elementos");
 
 	}
