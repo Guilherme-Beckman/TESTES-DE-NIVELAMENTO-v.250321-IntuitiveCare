eaf2c60d0a8d74e821060325f3d6c4388a2aaae2
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-29 14:21:41 -0400

    refactor: indentação

--- 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/CompressorService.java
+++ 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/CompressorService.java
@@ -17,34 +17,33 @@ public class CompressorService {
 
 	public File compressFiles(List<File> filesList, String absolutePath) {
 		File zipFile = new File(absolutePath);
-		
+
 		try (FileOutputStream fileOutputStream = new FileOutputStream(zipFile);
-			ZipOutputStream outputStream = new ZipOutputStream(fileOutputStream);	
-				){
+				ZipOutputStream outputStream = new ZipOutputStream(fileOutputStream);) {
 			for (File file : filesList) {
 				addFileOnZip(file, outputStream);
 			}
-			
+
 		} catch (IOException e) {
 			throw new ErrorWhileCompressFilesException();
-		} 
+		}
 		return zipFile;
 	}
 
 	private void addFileOnZip(File file, ZipOutputStream outputStream) throws IOException {
-		try (FileInputStream fileInputStream = new FileInputStream(file)){
+		try (FileInputStream fileInputStream = new FileInputStream(file)) {
 			ZipEntry zipEntry = new ZipEntry(file.getName());
 			outputStream.putNextEntry(zipEntry);
-			
+
 			byte[] buffer = new byte[1024];
 			int readBytes;
-			
-			while((readBytes = fileInputStream.read(buffer)) != -1) {
-				outputStream.write(buffer, 0 , readBytes);
+
+			while ((readBytes = fileInputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, readBytes);
 			}
 			outputStream.closeEntry();
-			
-	}
 
-}
+		}
+
+	}
 }
--- 1. TESTE DE WEB SCRAPING/webscraping/src/test/java/com/intituitivecare/webscraping/service/CompressorServiceTest.java
+++ 1. TESTE DE WEB SCRAPING/webscraping/src/test/java/com/intituitivecare/webscraping/service/CompressorServiceTest.java
@@ -1,6 +1,5 @@
 package com.intituitivecare.webscraping.service;
 
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -16,32 +15,29 @@ public class CompressorServiceTest {
 
 	@Test
 	public void shouldCompactFiles() throws IOException {
-		File file1 =  File.createTempFile("file1", ".pdf");
-		File file2 =  File.createTempFile("file2", ".pdf");
-		
-		
+		File file1 = File.createTempFile("file1", ".pdf");
+		File file2 = File.createTempFile("file2", ".pdf");
+
 		Files.writeString(file1.toPath(), "Conteudo arquivo 1");
 		Files.writeString(file2.toPath(), "Conteudo arquivo 2");
 		List<File> filesList = new ArrayList<>();
 		filesList.add(file1);
 		filesList.add(file2);
-		
-		File zipDir = File.createTempFile("anexos", ".zip");
-		
-		zipDir.delete();
-		CompressorService compressorService =  new CompressorService();
-		
-		File zipFile = compressorService.compressFiles(filesList, zipDir.getAbsolutePath());
-		
-        assertNotNull(zipFile, "O arquivo zip não deve ser nulo");
-        assertTrue(zipFile.exists(), "O arquivo zip deve existir");
-        assertTrue(zipFile.length() > 0, "O arquivo zio não deve estar vazio");
 
-        file1.delete();
-        file2.delete();
-        zipFile.delete();
-		
-		
-		
+		File zipDir = File.createTempFile("anexos", ".zip");
+
+		zipDir.delete();
+		CompressorService compressorService = new CompressorService();
+
+		File zipFile = compressorService.compressFiles(filesList, zipDir.getAbsolutePath());
+
+		assertNotNull(zipFile, "O arquivo zip não deve ser nulo");
+		assertTrue(zipFile.exists(), "O arquivo zip deve existir");
+		assertTrue(zipFile.length() > 0, "O arquivo zio não deve estar vazio");
+
+		file1.delete();
+		file2.delete();
+		zipFile.delete();
+
 	}
 }
