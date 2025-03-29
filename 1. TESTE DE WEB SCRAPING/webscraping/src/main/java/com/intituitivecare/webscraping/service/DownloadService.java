af42815e5b0c8f94310a5095c5fe9972f31452eb
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-29 16:45:25 -0400

    refactor: extrai a lógica de download de arquivos em métodos separados para melhor clareza

--- 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/DownloadService.java
+++ 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/DownloadService.java
@@ -3,6 +3,8 @@ package com.intituitivecare.webscraping.service;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -23,29 +25,13 @@ public class DownloadService {
 		this("downloads");
 	}
 	public List<File> downloadFiles(List<String> pdfLinks) {
+		List<File> downloadedFiles = new ArrayList<>();
 		try {
-			List<File> downloadedFiles = new ArrayList<>();
+
+			ensureDownloadDirectoryExists();
 			for (String pdfLink : pdfLinks) {
-				Path downloadPath = Path.of(downloadDir);
-				if(!Files.exists(downloadPath)) {
-					Files.createDirectories(downloadPath);
-				}
-				
-				String fileName = pdfLink.substring(pdfLink.lastIndexOf("/")+1);
-				
-				File outputFile = new File(downloadDir, fileName);
-				
-				try (BufferedInputStream in = new BufferedInputStream(new URL(pdfLink).openStream())){
-					FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
-					byte dataBuffer[] = new byte[1024];
-					int bytesRead;
-					
-					while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-						fileOutputStream.write(dataBuffer, 0, bytesRead);
-					}
-					fileOutputStream.close();
-				} 
-				downloadedFiles.add(outputFile);
+				File downloadedFile = downloadSingleFile(pdfLink);
+				downloadedFiles.add(downloadedFile);
 			}
 			
 			return downloadedFiles;
@@ -53,5 +39,32 @@ public class DownloadService {
 			throw new ErrorWhileDownloadFilesException();
 		}
 	}
+	
+	public void ensureDownloadDirectoryExists() throws IOException {
+		Path downloadPath = Path.of(downloadDir);
+		if(!Files.exists(downloadPath)) {
+			Files.createDirectories(downloadPath);
+		}
+	}
+	private File downloadSingleFile(String pdfLink) throws MalformedURLException, IOException {
+		String fileName = extractFileNameFromUrl(pdfLink);
+		
+		File outputFile = new File(downloadDir, fileName);
+		
+		try (BufferedInputStream in = new BufferedInputStream(new URL(pdfLink).openStream())){
+			FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+			byte dataBuffer[] = new byte[1024];
+			int bytesRead;
+			
+			while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+				fileOutputStream.write(dataBuffer, 0, bytesRead);
+			}
+			fileOutputStream.close();
+		}
+		return outputFile; 
+	}
+	  private String extractFileNameFromUrl(String url) {
+	        return url.substring(url.lastIndexOf("/") + 1);
+	    }
 
 }
