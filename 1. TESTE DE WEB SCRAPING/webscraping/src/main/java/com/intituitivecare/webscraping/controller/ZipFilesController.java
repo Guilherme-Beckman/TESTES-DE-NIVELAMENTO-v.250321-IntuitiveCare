163a04bc5e1daff74d65562353c0f9d8fbac4739
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-29 14:34:07 -0400

    feat: adiciona a classe ZipService e atualiza o ZipFilesController

--- 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/controller/ZipFilesController.java
+++ 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/controller/ZipFilesController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.intituitivecare.webscraping.service.ZipService;
+
 
 @RestController
 @RequestMapping("zip")
--- 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/ZipService.java
+++ 1. TESTE DE WEB SCRAPING/webscraping/src/main/java/com/intituitivecare/webscraping/service/ZipService.java
@@ -1,5 +1,14 @@
 package com.intituitivecare.webscraping.service;
 
+import java.io.File;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class ZipService {
+
+	public File getZipFIles() {
+		
+	}
 
 }
