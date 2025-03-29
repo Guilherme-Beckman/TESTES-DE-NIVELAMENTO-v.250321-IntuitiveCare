4fb69aa7060b43136594af56820321380d916e3b
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-29 14:07:04 -0400

    test: remove comentarios desnecessarios

--- 1. TESTE DE WEB SCRAPING/webscraping/src/test/java/com/intituitivecare/webscraping/service/CompressorServiceTest.java
+++ 1. TESTE DE WEB SCRAPING/webscraping/src/test/java/com/intituitivecare/webscraping/service/CompressorServiceTest.java
@@ -16,28 +16,21 @@ public class CompressorServiceTest {
 
 	@Test
 	public void shouldCompactFiles() throws IOException {
-		//criar arquivos temporarios
 		File file1 =  File.createTempFile("file1", ".pdf");
 		File file2 =  File.createTempFile("file2", ".pdf");
 		
 		
-		//escrever algo dentro dos arquivos temporarios
 		Files.writeString(file1.toPath(), "Conteudo arquivo 1");
 		Files.writeString(file2.toPath(), "Conteudo arquivo 2");
-		//criar uma lista com esses arquivos
 		List<File> filesList = new ArrayList<>();
 		filesList.add(file1);
 		filesList.add(file2);
 		
-		//criar um dir zip
 		File zipDir = File.createTempFile("anexos", ".zip");
 		
-		//deletar o conteudo do dir zip
 		zipDir.delete();
-		//instanciar o servico de compressao
 		CompressorService compressorService =  new CompressorService();
 		
-		//chamar o metodo e pegar o arquivo zipado
 		File zipFile = compressorService.compressFiles(filesList, zipDir.getAbsolutePath());
 		
         assertNotNull(zipFile, "O arquivo zip não deve ser nulo");
