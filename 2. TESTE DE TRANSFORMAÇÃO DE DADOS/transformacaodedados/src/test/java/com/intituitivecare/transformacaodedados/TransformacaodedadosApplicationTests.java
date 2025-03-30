57fe1c49a147aa2cf35b2cb7aac568db726e6e26
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-29 20:39:44 -0400

    build: inicia projeto spring boot

---
+++ 2. TESTE DE TRANSFORMAÇÃO DE DADOS/transformacaodedados/src/test/java/com/intituitivecare/transformacaodedados/TransformacaodedadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intituitivecare.transformacaodedados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformacaodedadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
