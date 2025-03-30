c87800b7b71d805fbbcc7c4718c3b46f198ab485
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-30 18:26:14 -0400

    feat: adiciona método writeCSV à classe CSVService para escrita de arquivos CSV

--- 2. TESTE DE TRANSFORMAÇÃO DE DADOS/transformacaodedados/src/main/java/com/intituitivecare/transformacaodedados/service/CSVService.java
+++ 2. TESTE DE TRANSFORMAÇÃO DE DADOS/transformacaodedados/src/main/java/com/intituitivecare/transformacaodedados/service/CSVService.java
@@ -1,5 +1,15 @@
 package com.intituitivecare.transformacaodedados.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class CSVService {
+
+	public void writeCSV(List<List<String>> table, String string) {
+		
+		
+	}
 
 }
