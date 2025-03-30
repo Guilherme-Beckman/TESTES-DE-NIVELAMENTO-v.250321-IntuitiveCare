eae8c614eb0b99dc19f6a28d58ba5911be3b7519
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-29 20:56:12 -0400

    feat: adiciona metodo inicial para os testes darem green

--- 2. TESTE DE TRANSFORMAÇÃO DE DADOS/transformacaodedados/src/main/java/com/intituitivecare/transformacaodedados/service/PDFExtractorService.java
+++ 2. TESTE DE TRANSFORMAÇÃO DE DADOS/transformacaodedados/src/main/java/com/intituitivecare/transformacaodedados/service/PDFExtractorService.java
@@ -1,8 +1,20 @@
 package com.intituitivecare.transformacaodedados.service;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class PDFExtractorService {
+
+	public List<String[]> extractTableData(File pdf) {
+		List<String[]> test = new ArrayList<>();
+		test.add(new String[] {"Procedimentos"});
+		test.add(new String[] {"dados, dados"});
+		
+		return test;
+	}
 
 }
