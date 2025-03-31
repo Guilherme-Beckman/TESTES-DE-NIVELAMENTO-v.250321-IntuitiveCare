78fa5103ce2f69de611c2b21ed89dddb8728fd1c
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-03-31 13:31:42 -0400

    refactor: remove comentarios

--- 2. TESTE DE TRANSFORMAÇÃO DE DADOS/transformacaodedados/src/main/java/com/intituitivecare/transformacaodedados/service/LegendExtractor.java
+++ 2. TESTE DE TRANSFORMAÇÃO DE DADOS/transformacaodedados/src/main/java/com/intituitivecare/transformacaodedados/service/LegendExtractor.java
@@ -5,22 +5,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LegendExtractor {
-
-    /**
-     * Extrai e armazena os mapeamentos da legenda a partir de um texto.
-     * Exemplo de entrada:
-     * "Legenda:\nOD: Seg. Odontológica\nAMB: Seg. Ambulatorial\nHCO: Seg. Hospitalar Com Obstetrícia\n..."
-     */
     public static Map<String, String> extractLegendMap(String legendText) {
         Map<String, String> legendMap = new HashMap<>();
-        
-        // Caso o texto contenha o cabeçalho "Legenda:", removemos-o
         legendText = legendText.replace("Legenda:", "").trim();
-        
-        // Dividindo o texto por linhas
         String[] lines = legendText.split("\\r?\\n");
-        
-        // Expressão regular para capturar: abreviação, dois pontos e a descrição
         Pattern pattern = Pattern.compile("^(\\w+)\\s*:\\s*(.+)$");
         
         for (String line : lines) {
