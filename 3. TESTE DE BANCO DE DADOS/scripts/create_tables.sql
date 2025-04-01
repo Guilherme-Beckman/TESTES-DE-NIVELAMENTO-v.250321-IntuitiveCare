a973665f0266f8b48851e01c87214cb15fa3b8e4
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-01 12:43:46 -0400

    feat: cria indexes para auxiliar na busca

--- 3. TESTE DE BANCO DE DADOS/scripts/create_tables.sql
+++ 3. TESTE DE BANCO DE DADOS/scripts/create_tables.sql
@@ -20,13 +20,7 @@ CREATE TABLE operadoras (
     regiao_comercializacao INTEGER,
     data_registro_ans DATE
 );
-INSERT INTO operadoras (registro_ans, cnpj, razao_social, nome_fantasia, modalidade, logradouro, numero, complemento, bairro, cidade, uf, cep, ddd, telefone, fax, endereco_eletronico, representante, cargo_representante, regiao_comercializacao, data_registro_ans)
-VALUES (
-    'Sem Registro', 'Sem CNPJ', 'Sem Razão Social', 'Sem Nome Fantasia', 'Sem Modalidade',
-    'Sem Logradouro', 'Sem Número', 'Sem Complemento', 'Sem Bairro', 'Sem Cidade', 'UF',
-    'Sem CEP', '000', 'Sem Telefone', 'Sem Fax', 'Sem Endereço Eletrônico',
-    'Sem Representante', 'Sem Cargo Representante', 0, '2023-10-10'
-);
+
 CREATE TABLE demonstracoes_contabeis (
     id SERIAL PRIMARY KEY,
     data DATE,
@@ -42,19 +36,3 @@ CREATE INDEX idx_demonstracoes_reg_ans ON demonstracoes_contabeis(reg_ans);
 CREATE INDEX idx_demonstracoes_conta ON demonstracoes_contabeis(cd_conta_contabil);
 CREATE INDEX idx_demonstracoes_data ON demonstracoes_contabeis(data);
 
-CREATE OR REPLACE FUNCTION trg_set_reg_ans_default()
-RETURNS trigger AS $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1 FROM operadoras WHERE registro_ans = NEW.reg_ans
-  ) THEN
-    NEW.reg_ans := 'Sem Registro';
-  END IF;
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER before_insert_demonstracoes
-BEFORE INSERT ON demonstracoes_contabeis
-FOR EACH ROW
-EXECUTE FUNCTION trg_set_reg_ans_default();
