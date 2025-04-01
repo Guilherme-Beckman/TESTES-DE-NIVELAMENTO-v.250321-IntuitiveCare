4dd5cdb8e149aa431429624e64f1c735c767b5f1
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-04-01 12:42:54 -0400

    feat: adiciona scrip para criação das tabelas

---
+++ 3. TESTE DE BANCO DE DADOS/scripts/create_tables.sql
@@ -0,0 +1,60 @@
+CREATE TABLE operadoras (
+    registro_ans VARCHAR(20) PRIMARY KEY,
+    cnpj VARCHAR(20),
+    razao_social VARCHAR(255),
+    nome_fantasia VARCHAR(255),
+    modalidade VARCHAR(100),
+    logradouro VARCHAR(255),
+    numero VARCHAR(20),
+    complemento VARCHAR(100),
+    bairro VARCHAR(100),
+    cidade VARCHAR(100),
+    uf VARCHAR(2),
+    cep VARCHAR(10),
+    ddd VARCHAR(5),
+    telefone VARCHAR(20),
+    fax VARCHAR(20),
+    endereco_eletronico VARCHAR(100),
+    representante VARCHAR(255),
+    cargo_representante VARCHAR(100),
+    regiao_comercializacao INTEGER,
+    data_registro_ans DATE
+);
+INSERT INTO operadoras (registro_ans, cnpj, razao_social, nome_fantasia, modalidade, logradouro, numero, complemento, bairro, cidade, uf, cep, ddd, telefone, fax, endereco_eletronico, representante, cargo_representante, regiao_comercializacao, data_registro_ans)
+VALUES (
+    'Sem Registro', 'Sem CNPJ', 'Sem Razão Social', 'Sem Nome Fantasia', 'Sem Modalidade',
+    'Sem Logradouro', 'Sem Número', 'Sem Complemento', 'Sem Bairro', 'Sem Cidade', 'UF',
+    'Sem CEP', '000', 'Sem Telefone', 'Sem Fax', 'Sem Endereço Eletrônico',
+    'Sem Representante', 'Sem Cargo Representante', 0, '2023-10-10'
+);
+CREATE TABLE demonstracoes_contabeis (
+    id SERIAL PRIMARY KEY,
+    data DATE,
+    reg_ans VARCHAR(20),
+    cd_conta_contabil VARCHAR(20),
+    descricao VARCHAR(255),
+    vl_saldo_inicial double precision,
+    vl_saldo_final double precision,
+    FOREIGN KEY (reg_ans) REFERENCES operadoras(registro_ans)
+);
+
+CREATE INDEX idx_demonstracoes_reg_ans ON demonstracoes_contabeis(reg_ans);
+CREATE INDEX idx_demonstracoes_conta ON demonstracoes_contabeis(cd_conta_contabil);
+CREATE INDEX idx_demonstracoes_data ON demonstracoes_contabeis(data);
+
+CREATE OR REPLACE FUNCTION trg_set_reg_ans_default()
+RETURNS trigger AS $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1 FROM operadoras WHERE registro_ans = NEW.reg_ans
+  ) THEN
+    NEW.reg_ans := 'Sem Registro';
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER before_insert_demonstracoes
+BEFORE INSERT ON demonstracoes_contabeis
+FOR EACH ROW
+EXECUTE FUNCTION trg_set_reg_ans_default();
